5b0cb128faf5e0d46d8efb510290a43d6bd4b31b
yejiiny <156687942+yejiiny@users.noreply.github.com>
2024-01-17 15:23:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+if n % 5 == 0:
+    print(n//5)
+else:
+    cnt = 0
+    while n > 0:
+        n -= 3
+        cnt += 1
+        if n % 5 == 0:
+            print(cnt + n//5)
+            break
+        elif n == 1 or n == 2:
+            print(-1)
+            break
+        elif n == 0:
+            print(cnt)
+            break
